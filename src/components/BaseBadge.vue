a91759c483b0b99f242d018e55f6b3ea8b6008a3
Ben Hong <ben@bencodezen.io>
2020-06-13 09:47:14 -0400

    feature: add base-badge example

---
+++ src/components/BaseBadge.vue
@@ -0,0 +1,69 @@
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    borderSide: {
+      type: String,
+      required: false
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="badge" :class="`is-${type} has-border-${borderSide}`">
+    {{ text }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.badge {
+  padding: 0.5rem 0.75rem;
+  background-color: #ccc;
+  border-radius: 3px;
+  color: #222;
+
+  &.has-border-left {
+    border-left: 8px solid #999;
+  }
+
+  &.has-border-right {
+    border-right: 8px solid #999;
+  }
+
+  &.has-border-top {
+    border-top: 8px solid #999;
+  }
+
+  &.has-border-bottom {
+    border-bottom: 8px solid #999;
+  }
+
+  &.is-info {
+    border-color: rgb(11, 73, 155);
+    background-color: rgb(174, 204, 243);
+  }
+
+  &.is-success {
+    border-color: rgb(35, 155, 11);
+    background-color: rgb(174, 243, 174);
+  }
+
+  &.is-warning {
+    border-color: rgb(155, 145, 11);
+    background-color: rgb(242, 243, 174);
+  }
+
+  &.is-danger {
+    border-color: rgb(155, 11, 11);
+    background-color: rgb(245, 166, 166);
+  }
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,12 +11,47 @@
         >Scalable Prop Patterns GitHub repository</a
       >.
     </p>
+    <h2>Here are my badges:</h2>
+    <ul class="badge-list">
+      <li v-for="badge in badges" :key="badge.text" class="badge-list-item">
+        <BaseBadge
+          :type="badge.type"
+          :text="badge.text"
+          :border-side="badge.borderSide"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import BaseBadge from '@/components/BaseBadge'
+
 export default {
-  name: 'HelloWorld'
+  name: 'HelloWorld',
+  components: {
+    BaseBadge
+  },
+  data: () => ({
+    badges: [
+      { type: 'info', text: 'Info - Left', borderSide: 'left' },
+      { type: 'info', text: 'Info - Right', borderSide: 'right' },
+      { type: 'info', text: 'Info - Top', borderSide: 'top' },
+      { type: 'info', text: 'Info - Bottom', borderSide: 'bottom' },
+      { type: 'success', text: 'Success - Left', borderSide: 'left' },
+      { type: 'success', text: 'Success - Right', borderSide: 'right' },
+      { type: 'success', text: 'Success - Top', borderSide: 'top' },
+      { type: 'success', text: 'Success - Bottom', borderSide: 'bottom' },
+      { type: 'warning', text: 'Warning - Left', borderSide: 'left' },
+      { type: 'warning', text: 'Warning - Right', borderSide: 'right' },
+      { type: 'warning', text: 'Warning - Top', borderSide: 'top' },
+      { type: 'warning', text: 'Warning - Bottom', borderSide: 'bottom' },
+      { type: 'danger', text: 'Danger - Left', borderSide: 'left' },
+      { type: 'danger', text: 'Danger - Right', borderSide: 'right' },
+      { type: 'danger', text: 'Danger - Top', borderSide: 'top' },
+      { type: 'danger', text: 'Danger - Bottom', borderSide: 'bottom' }
+    ]
+  })
 }
 </script>
 
@@ -35,5 +70,16 @@ li {
 }
 a {
   color: #338b64;
+}
+
+.badge-list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+}
+
+.badge-list-item {
+  margin-bottom: 1rem;
 }
 </style>
