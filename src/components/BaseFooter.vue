0d487a8d1090b0492fd0afcc02eca2b127fe2a45
Ben Hong <ben@bencodezen.io>
2020-06-13 08:37:04 -0400

    feature: add BaseFooter example

---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  props: ['brand', 'copyrightYear', 'navLinks']
+}
+</script>
+
+<template>
+  <footer class="footer">
+    <ul class="footer-nav-list">
+      <li v-for="link in navLinks" :key="link.label">
+        <a class="footer-nav-link" href="#">{{ link.label }}</a>
+      </li>
+    </ul>
+    {{ brand }} © {{ copyrightYear }}
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  margin-top: 2rem;
+  padding-top: 1rem;
+  padding-bottom: 2rem;
+  border-top: 2px solid #ccc;
+}
+
+.footer-nav-list {
+  display: flex;
+  justify-content: center;
+}
+
+.footer-nav-list {
+  list-style: none;
+}
+
+.footer-nav-link {
+  text-decoration: none;
+  padding-right: 1rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <BaseFooter
+      brand="Company Name"
+      copyright-year="2020"
+      :nav-links="[
+        { label: 'Home' },
+        { label: 'About' },
+        { label: 'Product' },
+        { label: 'Contact' }
+      ]"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import BaseFooter from '@/components/BaseFooter.vue'
 
 export default {
   name: 'Home',
   components: {
+    BaseFooter,
     HelloWorld
   }
 }
